a6f667a1824b9eac4469d3e88a59f6f931ea3227
Daniel <dansto13@yahoo.com>
2018-02-02 19:38:09 +0100

    Setup frontend.

--- app.js
+++ app.js
@@ -3,15 +3,19 @@ const app = express();
 const bodyParser = require("body-parser");
 const port = process.env.PORT || 3000;
 
-// use body-parser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
 }));
 
+app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/views'));
+
 const todoRoutes = require('./routes/todos');
 
-app.get('/', (req, res) => res.send('Hey there from root route!'));
+app.get('/', (req, res) => {
+  res.sendFile('index.html');
+});
 
 app.use('/api/todos', todoRoutes);
 
